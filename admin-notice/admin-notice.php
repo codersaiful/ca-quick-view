5d0828b1b82580fc23ea488ad9f11efdaff746a4
Saiful Islam <codersaiful@gmail.com>
2022-04-21 12:13:51 +0600

    working

--- admin-notice/admin-notice.php
+++ admin-notice/admin-notice.php
@@ -75,13 +75,18 @@ class CA_Admin_Notice
      * Notice show if condition meet
      */
 	public function show_notification_on_time(){
-        //$get_option             = get_option("ca_admin_notice");
-        $ca_notice_close_date   = get_option("ca_notice_close_date");
-        $close_date		        = date("Y-m-d", $ca_notice_close_date);
+        
+        $ca_notice_close_date   = get_option( self::PREFIX . "notice_close_date");
+        if( ! empty($ca_notice_close_date) && is_numeric( $ca_notice_close_date )){
+            $close_date		        = date("Y-m-d", $ca_notice_close_date);
 
-		$date				    = new DateTime($close_date);
-		$now 				    = new DateTime();
-		$date_diff 			    = $date->diff($now)->format("%d");
+            $date				    = new DateTime($close_date);
+            $now 				    = new DateTime();
+            $date_diff = $date->diff($now)->format("%d");
+        }else{
+            $date_diff = 99999;
+        }
+        			    
         
         //Manual value for test
 		//$date_diff 			    = 15;
@@ -89,7 +94,7 @@ class CA_Admin_Notice
        /*  if( $get_option!=self::PREFIX .'_dont_show' ){
 			add_action("admin_notices", [$this, "notice_output4"]);
 		} */
-        if($date_diff>= 5 ){
+        if( $date_diff>= 5 ){
 			add_action("admin_notices", [$this, "notice_output4"]);
 		}
        
