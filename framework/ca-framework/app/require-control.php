7c4d7a4990eef9dd663c9e170700b4600775c646
Saiful Islam <codersaiful@gmail.com>
2022-05-19 10:42:56 +0600

    required plugin issue has been solved

--- framework/ca-framework/app/require-control.php
+++ framework/ca-framework/app/require-control.php
@@ -84,23 +84,25 @@ if( ! class_exists( 'CA_Framework\Require_Control' ) ){
 
         public function run()
         {
-            
+            // Return null, If already our required plugin is installed.
+            if( is_plugin_active( $this->plugin_slug ) ){ 
+                return;
+            }
+
+            if( $this->required && ! is_plugin_active( $this->plugin_slug ) ){
+                $this->stop_next = 1;
+                return;
+            };
+
             //Check Admin User
-            if( ! is_admin() ){
-                $this->stop_next = 1; 
+            if( ! is_admin() ){ 
                 return;
             }
             
-
-            // Return null, If already our required plugin is installed.
-            if( is_plugin_active( $this->plugin_slug ) ) return;
-            
-           
-
             //Return Null Controll;
             if( isset( $_GET['action'] ) && ( $_GET['action'] == 'install-plugin' || $_GET['action'] == 'activate' ) ){
                 $this->stop_next = 1;
-                return $this->stop_next;
+                return;
             }
 
             //Check Aganin installation prosibility when reconneded and Date over. by default we set diff_limit = 5 days.
@@ -108,9 +110,7 @@ if( ! class_exists( 'CA_Framework\Require_Control' ) ){
 
             //Final Display Notice 
             add_action( 'admin_notices', [ $this, 'display_notice' ] );
-
-            if( $this->required ) $this->stop_next = 1;
-            return $this->stop_next;
+            return;
         }
 
         /**
