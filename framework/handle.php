d8e5f722e7c6dca37acad03937b9c778575df87e
Saiful Islam <codersaiful@gmail.com>
2022-08-23 09:40:05 +0600

    message fix

--- framework/handle.php
+++ framework/handle.php
@@ -10,8 +10,8 @@ $r_slug = 'woo-product-table/woo-product-table.php';
 $t_slug = 'ca-quick-view/init.php';
 $req = new Require_Control($r_slug,$t_slug);
 $req->set_args(['Name'=>'Product Table Plugin for WooCommerce'])
-->set_message("this sis is  sdisd sdodso")
-->set_required()
+->set_message("Product Table Plugin is a famouse plugin for displaying product in Table View.")
+// ->set_required()
 ->run();
 
 $not = new Notice('s3333ld');
